cf3f21c123d455a85bfbc7224305525a6475bc35
Varun Mohan <varunmadhavam@gmail.com>
2022-06-14 11:08:39 +0530

    added waveform support

--- project_template/project_template.v
+++ project_template/project_template.v
@@ -2,4 +2,9 @@ module project_template(a,b,c);
        input a,b;
        output c;
        assign c=a|b;
+       initial 
+              begin
+                     $dumpfile("project_template.vcd");
+                     $dumpvars(0,project_template);
+              end
 endmodule
