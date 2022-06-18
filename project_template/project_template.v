94505883d2b77f17276cd94bea8b53a9e8222859
Varun Mohan <varunmadhavam@gmail.com>
2022-06-18 09:53:08 +0530

    updated project template

--- project_template/project_template.v
+++ project_template/project_template.v
@@ -2,9 +2,4 @@ module project_template(a,b,c);
        input a,b;
        output c;
        assign c=a|b;
-       initial 
-              begin
-                     $dumpfile("project_template.vcd");
-                     $dumpvars(0,project_template);
-              end
 endmodule
